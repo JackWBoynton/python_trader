11d4704c48822de965031c7a057883a63d365bfe
JackWBoynton <jboynton1999@gmail.com>
2019-09-15 11:09:58 -0400

    a

--- helper.py
+++ helper.py
@@ -27,7 +27,7 @@ def load_dfs(asset, files):
     frm = files[0].split('/')[1].split('.')[0]
     too = files[-1].split('/')[1].split('.')[0]
     print('backtest dates: ' + frm + '-' + too)
-    if not glob.glob('../loaded'+frm+too+'.csv'):
+    if not glob.glob('../../loaded'+frm+too+'.csv'):
         a = []
         first = True
         for i in tqdm(files):
@@ -38,9 +38,9 @@ def load_dfs(asset, files):
                 first = False
                 a = data
 
-        a.to_csv(path_or_buf='../loaded'+frm+too+'.csv', header=False)
+        a.to_csv(path_or_buf='../../loaded'+frm+too+'.csv', header=False)
     else:
-        a = pd.read_csv('../loaded'+frm+too+'.csv', header=None, low_memory=False, dtype={
+        a = pd.read_csv('../../loaded'+frm+too+'.csv', header=None, low_memory=False, dtype={
                            1: float}, usecols=[1])
     print('loaded ' + str(a.shape[0]) + ' ticks of data')
     return a
@@ -51,13 +51,13 @@ def load_dfs_mult(asset, files):
     frm = files[0].split('/')[1].split('.')[0]
     too = files[-1].split('/')[1].split('.')[0]
     print('backtest dates: ' + frm + '-' + too)
-    if not glob.glob('../loaded'+frm+too+'.csv'):
+    if not glob.glob('../../loaded'+frm+too+'.csv'):
         with Pool(processes=8) as pool:
             df_list = (pool.map(load_df, tqdm(files)))
             combined = pd.concat(tqdm(df_list), ignore_index=True)
-            combined.to_csv(path_or_buf='../loaded'+frm+too+'.csv', header=False)
+            combined.to_csv(path_or_buf='../../loaded'+frm+too+'.csv', header=False)
     else:
-        combined = pd.read_csv('../loaded'+frm+too+'.csv', header=None, low_memory=False, dtype={
+        combined = pd.read_csv('../../loaded'+frm+too+'.csv', header=None, low_memory=False, dtype={
                            1: float}, usecols=[1])
     print('loaded ' + str(combined.shape[0]) + ' ticks of data')
     return combined
