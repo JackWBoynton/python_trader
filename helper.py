afc5fb5925e0e1d82405043c6f541da082c8f95e
jayceslesar <47452474+jayceslesar@users.noreply.github.com>
2019-09-21 16:11:46 -0400

    final housekeeping

--- helper.py
+++ helper.py
@@ -3,23 +3,20 @@ import glob
 from tqdm import tqdm
 from multiprocessing import Pool
 
+
 def load_df(filename):
     asset = 'XBTU19'
-    data = pd.read_csv(filename, header=None, low_memory=False, dtype={3: float}, usecols=[0, 1, 3], skiprows=2)
-
+    data = pd.read_csv(filename, header=None, low_memory=False, dtype={
+                       3: float}, usecols=[0, 1, 3], skiprows=2)
     for n, i in enumerate(data[1]):
         if i == asset:
             data = pd.DataFrame(data.values[n:])
             break
-
     for n, j in enumerate(data[1]):
         if j != asset:
             data = pd.DataFrame(data.values[:n])
             break
-    #del data[0]
-
     del data[1]
-    #print(data.head())
     return data
 
 
@@ -27,7 +24,7 @@ def load_dfs(asset, files):
     frm = files[0].split('/')[1].split('.')[0]
     too = files[-1].split('/')[1].split('.')[0]
     print('backtest dates: ' + frm + '-' + too)
-    if 1 == 1 or not glob.glob('../loaded'+frm+too+'.csv'):
+    if 1 == 1 or not glob.glob('../loaded' + frm + too + '.csv'):
         a = []
         first = True
         for i in tqdm(files):
@@ -37,10 +34,10 @@ def load_dfs(asset, files):
             else:
                 first = False
                 a = data
-
-        a.to_csv(path_or_buf='../loaded'+frm+too+'.csv', header=False)
+        a.to_csv(path_or_buf='../loaded' + frm + too + '.csv', header=False)
     else:
-        a = pd.read_csv('../loaded'+frm+too+'.csv', header=None, low_memory=False, dtype={1: float}, usecols=[0, 1])
+        a = pd.read_csv('../loaded' + frm + too + '.csv', header=None,
+                        low_memory=False, dtype={1: float}, usecols=[0, 1])
     print('loaded ' + str(a.shape[0]) + ' ticks of data')
     return a
 
@@ -51,14 +48,14 @@ def load_dfs_mult(asset, files):
     too = files[-1].split('/')[1].split('.')[0]
     files.reverse()
     print('backtest dates: ' + frm + '-' + too)
-    if 1==1 or not glob.glob('../loaded'+frm+too+'.csv'):
+    if 1 == 1 or not glob.glob('../loaded' + frm + too + '.csv'):
         with Pool(processes=8) as pool:
             df_list = (pool.map(load_df, files))
             combined = pd.concat(df_list, ignore_index=True)
-            combined.to_csv(path_or_buf='../loaded'+frm+too+'.csv', header=False)
+            combined.to_csv(path_or_buf='../loaded' +
+                            frm + too + '.csv', header=False)
     else:
-        combined = pd.read_csv('../loaded'+frm+too+'.csv', header=None, low_memory=False, dtype={1: float}, usecols=[0, 1])
+        combined = pd.read_csv('../loaded' + frm + too + '.csv', header=None,
+                               low_memory=False, dtype={1: float}, usecols=[0, 1])
     print('loaded ' + str(combined.shape[0]) + ' ticks of data')
-    #print (combined.head())
-    #print (combined.tail())
     return combined
