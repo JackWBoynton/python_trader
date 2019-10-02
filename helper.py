1ac9900d5e1e7e717c7aa7c4ff31903420098119
JackWBoynton <jboynton1999@gmail.com>
2019-10-02 11:20:58 -0400

    a

--- helper.py
+++ helper.py
@@ -8,7 +8,7 @@ def load_df(filename):
     asset = 'XBTU19'
     data = pd.read_csv(filename, header=None, low_memory=False, dtype={
                        3: float}, usecols=[0, 1, 3], skiprows=2, na_values=0)
-    print(filename)
+    print(filename, data.head())
     for n, i in enumerate(data[1]):
         if i == asset:
             data = pd.DataFrame(data.values[n:])
