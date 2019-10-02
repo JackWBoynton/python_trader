ad77b7f5663d78c703e6d903fb77291cec842f67
JackWBoynton <jboynton1999@gmail.com>
2019-10-02 10:59:53 -0400

    a

--- helper.py
+++ helper.py
@@ -44,9 +44,7 @@ def load_dfs(asset, files):
 
 def load_dfs_mult(asset, files):
     # multiprocessing version of load_dfs
-    for n, i in enumerate(files):
-        if i.split('/')[1].split('.')[0] is "20190927":
-            del files[n]
+    
     del files[0]
     frm = files[0].split('/')[1].split('.')[0]
     too = files[-1].split('/')[1].split('.')[0]
