4edeec452be12a8002919cefecaf6fcea51a5471
JackWBoynton <jboynton1999@gmail.com>
2019-10-02 21:49:43 -0400

    a

--- pyrenko.py
+++ pyrenko.py
@@ -328,9 +328,9 @@ class renko:
                 if self.runs > 0:
                     self.close_short(self.pricea)
                     if self.long:
-                        side = 1
-                    elif self.short:
                         side = 0
+                    elif self.short:
+                        side = 1
                     if len(self.renko_prices) > 10:
                         new_trade(past_bricks=self.renko_prices[-10:], price_open=self.open, price_close=self.pricea, side=side, macd_open=self.macd_open, macd_close=self.macd()[-1], sma_open=self.sma_open, sma_close=self.sma()[-1], time_open=self.open_time, time_close=self.act_timestamps[ind])
                 if self.end_backtest <= self.last_timestamp and not self.j_backtest and len(self.ys) > 35:
@@ -363,6 +363,12 @@ class renko:
                 self.long = False
                 if self.runs > 0:
                     self.close_long(self.pricea)
+                    if self.long:
+                        side = 1
+                    elif self.short:
+                        side = 0
+                    if len(self.renko_prices) > 10:
+                        new_trade(past_bricks=self.renko_prices[-10:], price_open=self.open, price_close=self.pricea, side=side, macd_open=self.macd_open, macd_close=self.macd()[-1], sma_open=self.sma_open, sma_close=self.sma()[-1], time_open=self.open_time, time_close=self.act_timestamps[ind])
 
                 if self.end_backtest <= self.last_timestamp and not self.j_backtest and len(self.ys) > 35:
                     threading.Thread(target=self.trade.sell_short,
