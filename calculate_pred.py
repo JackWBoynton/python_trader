3ceeaac555971647c27ece6d4c8924df38127b8a
JackWBoynton <jboynton1999@gmail.com>
2019-10-04 15:35:27 -0400

    fix

--- calculate_pred.py
+++ calculate_pred.py
@@ -1,6 +1,6 @@
 import xgboost as xgb
 from sklearn.externals import joblib
-scaler_filename = "scaler.save"
+scaler_filename = "../scaler.save"
 scaler = joblib.load(scaler_filename)
 
 bst = xgb.Booster({'nthread': 4})  # init model
