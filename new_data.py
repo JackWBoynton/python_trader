b2788cca9a039863aa73d443e1174ff8f8a6d482
JackWBoynton <jboynton1999@gmail.com>
2019-10-05 14:58:03 -0400

    fix

--- new_data.py
+++ new_data.py
@@ -11,16 +11,16 @@ def download_new(location):
     if '../'+date.strftime("%Y%m%d")+'.csv' not in glob.glob('../2019*.csv'):
         print('downloading new days data')
         wget.download(url="https://s3-eu-west-1.amazonaws.com/public.bitmex.com/data/quote/{}.csv.gz".format(date.strftime('%Y%m%d')), out=location)
-        input = gzip.GzipFile("../"+date.strftime("%Y%m%d")+'.csv.gz', 'rb')
+        input = gzip.GzipFile(location+date.strftime("%Y%m%d")+'.csv.gz', 'rb')
         s = input.read()
         input.close()
-        output = open("../"+date.strftime("%Y%m%d")+'.csv', 'wb')
+        output = open(location+date.strftime("%Y%m%d")+'.csv', 'wb')
         output.write(s)
         output.close()
         ### Parse:
         print('parsing')
         asset = 'XBTUSD'
-        data = pd.read_csv("../"+date.strftime("%Y%m%d")+'.csv', header=None, low_memory=False, usecols=[0, 1, 3], dtype={0: str, 1: str, 3: float}, skiprows=2)
+        data = pd.read_csv(location+date.strftime("%Y%m%d")+'.csv', header=None, low_memory=False, usecols=[0, 1, 3], dtype={0: str, 1: str, 3: float}, skiprows=2)
         for n, j in enumerate(data[1]):
             if j == asset:
                 data = pd.DataFrame(data.values[n:])
@@ -30,7 +30,7 @@ def download_new(location):
                 data = pd.DataFrame(data.values[:n])
                 break
         del data[1]
-        data.to_csv("../"+date.strftime("%Y%m%d")+'.csv')
+        data.to_csv(location+date.strftime("%Y%m%d")+'.csv')
         print('done')
         return 1
     else:
