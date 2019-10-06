0b7b08979d4474aa16c01ed5050d99fdd4eec54c
JackWBoynton <jboynton1999@gmail.com>
2019-10-06 12:27:04 -0400

    a

--- helper.py
+++ helper.py
@@ -7,30 +7,6 @@ len_df = 0
 files_ = []
 
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-def load_df(filename):
-    asset = 'XBTU19'
-    data = pd.read_csv(filename, header=0, low_memory=False, dtype={
-                       'timestamp': str, 'symbol': str, 'bidPrice': float}, usecols=['timestamp', 'symbol', 'bidPrice'], skiprows=0, na_values=0)
-    #print(data.columns, data[data.columns[1]])
-    for n, i in enumerate(data['symbol']):
-        if i == asset:
-            data = pd.DataFrame(data.values[n:])
-            #print (data)
-            break
-    for n, j in enumerate(data['symbol']):
-        if j != asset:
-            data = pd.DataFrame(data.values[:n])
-            break
-<<<<<<< HEAD
-    print (data.head())
-    #del data['symbol']
-=======
-    del data[1]
-=======
->>>>>>> 6fdf63a5ffcc42f80726eb45fe097f2d103a539d
 def load_df(ind, filename):
     global len_df
     len_df += 1
@@ -38,11 +14,6 @@ def load_df(ind, filename):
 
     data = pd.read_csv(filename, header=None, low_memory=True, dtype={0: str, 1: str,
                        3: float}, skiprows=2, na_values=0).progress_apply(lambda x: x)
-<<<<<<< HEAD
-=======
->>>>>>> new
->>>>>>> cef4d090c829a91a06e88ae3497d390fba65ab07
->>>>>>> 6fdf63a5ffcc42f80726eb45fe097f2d103a539d
     return data
 
 
@@ -71,13 +42,6 @@ def load_dfs(asset, files):
 def load_dfs_mult(asset, files, location):
     download_new(location)
     # multiprocessing version of load_dfs
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-    frm = files[0].split('/')[1].split('.')[0]
-    too = files[-1].split('/')[1].split('.')[0]
-=======
->>>>>>> 6fdf63a5ffcc42f80726eb45fe097f2d103a539d
     for n, i in enumerate(files):
         if location == '../':
             if i.split('/')[1].split('.')[0] == '20190927':
@@ -89,19 +53,13 @@ def load_dfs_mult(asset, files, location):
                 del files[n]
             frm = files[0].split('.')[0]
             too = files[-1].split('.')[0]
-<<<<<<< HEAD
-=======
->>>>>>> new
->>>>>>> 6fdf63a5ffcc42f80726eb45fe097f2d103a539d
 
     files.reverse()
-    print (files)
     print('backtest dates: ' + frm + '-' + too)
     global files_
     files_ = files
     if 1 == 1 or not glob.glob(location+'loaded' + frm + too + '.csv'):
         with Pool(processes=8) as pool:
-<<<<<<< HEAD
             df_list = (pool.starmap(load_df, enumerate(files)))
             tqdm.pandas(desc="concat csvs")
             combined = pd.concat(df_list, ignore_index=True).progress_apply(lambda x: x)  # apply dummy lambda fn to call tqdm.pandas()
@@ -109,26 +67,6 @@ def load_dfs_mult(asset, files, location):
                             frm + too + '.csv', header=False)
     else:
         combined = pd.read_csv(location+'loaded' + frm + too + '.csv', header=None,
-=======
-<<<<<<< HEAD
-            df_list = (pool.map(load_df, files))
-            print(df_list)
-            combined = pd.concat(df_list, ignore_index=True)
-            print(combined)
-            combined.to_csv(path_or_buf='../loaded' +
-                            frm + too + '.csv', header=0)
-    else:
-        combined = pd.read_csv('../loaded' + frm + too + '.csv', header=None,
-=======
-            df_list = (pool.starmap(load_df, enumerate(files)))
-            tqdm.pandas(desc="concat csvs")
-            combined = pd.concat(df_list, ignore_index=True).progress_apply(lambda x: x)  # apply dummy lambda fn to call tqdm.pandas()
-            combined.to_csv(path_or_buf=location+'loaded' +
-                            frm + too + '.csv', header=False)
-    else:
-        combined = pd.read_csv(location+'loaded' + frm + too + '.csv', header=None,
->>>>>>> new
->>>>>>> 6fdf63a5ffcc42f80726eb45fe097f2d103a539d
                                low_memory=False, dtype={1: float}, usecols=[0, 1], skiprows=2, na_values=0)
     print('loaded ' + str(combined.shape[0]) + ' ticks of data')
     return combined
