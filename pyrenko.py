455cb8a22cd7e34a651871f3168c4026e746ae18
JackWBoynton <jboynton1999@gmail.com>
2019-10-05 21:38:45 -0400

    a

--- pyrenko.py
+++ pyrenko.py
@@ -245,9 +245,9 @@ class renko:
 
     def close_short(self, price):
         # calculates profit on close of short trade
-        net = round(((1 / self.pricea - 1 / (self.open)) * floor(self.risk*self.open)*self.leverage)[0], 8)
+        net = round(((1 / self.pricea - 1 / (self.open)) * floor(self.risk*self.open)*self.leverage), 8)
         self.profit += net
-        fee = round(((1 / self.pricea - 1 / (self.open)) * floor(self.risk*self.open)*self.leverage * self.backtest_fee)[0], 8)
+        fee = round(((1 / self.pricea - 1 / (self.open)) * floor(self.risk*self.open)*self.leverage * self.backtest_fee), 8)
         self.profit -= fee
         self.backtest_bal_usd += round((net - fee)[0], 8)
         ret = round((net - fee)[0], 8)/self.risk
