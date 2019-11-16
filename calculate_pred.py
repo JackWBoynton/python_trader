577d747b958cee1cbb95b0e6cf988f497f51c930
JackWBoynton <jboynton1999@gmail.com>
2019-11-16 18:46:51 -0500

    asd

--- calculate_pred.py
+++ calculate_pred.py
@@ -5,7 +5,7 @@ scaler_filename = "scaler.save"
 scaler = joblib.load(scaler_filename)
 
 bst = xgb.Booster({'nthread': 4})  # init model
-bst.load_model('../0001.model')  # load data
+bst.load_model('0001.model')  # load data
 
 
 def main(ys, macd, sma, last_price):
